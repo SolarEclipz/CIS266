205b20732ccea9a701c4de504863c3000bdcccad
SolarEclipz <SolarEclipz@users.noreply.github.com>
2019-02-06 00:01:03 -0600

    Create ProductDB_API.php
    Unit 1 Ex 3 - ../sp/ProductDB_API - API for ../sp/cl/productdb.php

---
+++ Unit 1 - Overview of Web Services/Ex 3 - DB Emulator RESTful AJAX/ServiceProvider/ProductDB_API.php
@@ -0,0 +1,19 @@
+<?php
+//header("Content-type: application/json");
+include('class_lib/ProductDB.php');
+
+$productName = $_REQUEST['product'];
+
+$productDB = new ProductDB();
+$rProduct = $productDB->getProduct($productName);
+
+$data = array();
+$data['productString'] = $rProduct->toString();
+$data['productCode'] = $rProduct->getCode();
+$data['productDescription'] = $rProduct->getDescription();
+$data['productPublisher'] = $rProduct->getPublisher();
+$data['productPrice'] = $rProduct->getPrice();
+
+print(json_encode($data));
+
+?>
